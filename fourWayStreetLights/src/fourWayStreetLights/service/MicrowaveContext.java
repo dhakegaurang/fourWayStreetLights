d6db3a83baa5663d058265c616e6e661eab986eb
dhakegaurang <dhakegaurang.com>
2018-06-22 00:26:58 -0400

    Created methods in stateInterface and modified MicrowaveContext class to add and remove vehicle

--- fourWayStreetLights/src/fourWayStreetLights/service/MicrowaveContext.java
+++ fourWayStreetLights/src/fourWayStreetLights/service/MicrowaveContext.java
@@ -1,5 +1,65 @@
 package fourWayStreetLights.service;
 
-public class MicrowaveContext {
+import java.util.LinkedList;
+import java.util.Queue;
 
+import microwaveOven.service.Car;
+
+public class MicrowaveContext implements MicrowaveStateI{
+	
+	private States currentState;
+	private enum States{
+		nothToAllRed, northToAllGreen, southToAllRed, southToAllGreen, eastToAllRed, eastToAllGreen, westToAllRed, westToAllGreen;
+		
+		public States getNext() {
+		     return this.ordinal() < States.values().length - 1
+		         ? States.values()[this.ordinal() + 1]
+		         : null;
+		}
+	}
+	private Queue<Car> carQueue = new LinkedList<>();
+	
+	@Override
+	public void addVehicle(String vehicleNumber, String inDirection) {
+		carQueue.add(new Car(vehicleNumber, inDirection));
+		
+	}
+	
+	@Override
+	public void moveVehicle(String vehicleNumber) {
+		for(Car car : carQueue) {
+			if(car.getCarNumber().equals(vehicleNumber)) {
+				carQueue.remove(car);
+			}
+		}
+	}
+	
+	@Override
+	public void twoCarPasses() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void immediateTransitionFromGreenToRed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Queue<Car> getCarQueue() {
+		return carQueue;
+	}
+
+	public void setCarQueue(Queue<Car> carQueue) {
+		this.carQueue = carQueue;
+	}
+
+	public States getCurrentState() {
+		return currentState;
+	}
+
+	public void setCurrentState(States currentState) {
+		this.currentState = currentState;
+	}
+	
 }
--- fourWayStreetLights/src/fourWayStreetLights/service/MicrowaveStateI.java
+++ fourWayStreetLights/src/fourWayStreetLights/service/MicrowaveStateI.java
@@ -1,5 +1,16 @@
 package fourWayStreetLights.service;
 
 public interface MicrowaveStateI {
-
+	// Adding the vehicle
+	void addVehicle(String vehicleNumber, String direction);
+	
+	// Moving vehicle when green
+	void moveVehicle(String vehicleNumber);
+	
+	// When signal becomes green, then before it turns red, two cars should go through
+	void twoCarPasses();
+	
+	// After two car passes through green signal, then current signal becomes red and other signal becomes green
+	void immediateTransitionFromGreenToRed();
+	
 }
